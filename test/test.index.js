65c7f787c7415e627e6a2060421fe84bf513a733
#云淡然 <cloudcome@163.com>
2016-05-18 11:30:57 +0800

    blear.utils.compatible

--- test/test.index.js
+++ test/test.index.js
@@ -7,79 +7,74 @@
 
 'use strict';
 
-var collection = require('../src/index.js');
+var compatible = require('../src/index.js');
 
 describe('index.js', function () {
-    it('.each', function () {
-        var obj1 = {a: 1, b: 2};
-        collection.each(obj1, function (key, val) {
-            expect(obj1[key]).toBe(val);
-        });
+    var divEl = null;
 
-        var arr1 = ['a', 'b'];
-        collection.each(arr1, function (index, val) {
-            expect(arr1[index]).toBe(val);
-        });
-
-        var boo1 = true;
-        var times = 0;
-        collection.each(boo1, function () {
-            times++;
-        });
-        expect(times).toEqual(0);
+    beforeAll(function () {
+        divEl = document.createElement('div');
+        document.body.appendChild(divEl);
     });
 
-    it('.map', function () {
-        var obj1 = {a: 1, b: 2};
-        var obj2 = collection.map(obj1, function (val) {
-            return val * val * val;
-        });
-        expect(obj1).not.toBe(obj2);
-        expect(obj1).toEqual({a: 1, b: 2});
-        expect(obj2).toEqual({a: 1, b: 8});
-
-        var arr1 = ['a', 'b'];
-        var arr2 = collection.map(arr1, function (val) {
-            return val + val + val;
-        });
-        expect(arr1).not.toBe(arr2);
-        expect(arr1).toEqual(['a', 'b']);
-        expect(arr2).toEqual(['aaa', 'bbb']);
-
-        var boo1 = true;
-        var times = 0;
-        var boo2 = collection.map(boo1, function () {
-            times++;
-            return '';
-        });
-        expect(times).toEqual(0);
-        expect(boo1).toBe(boo2);
+    afterAll(function () {
+        document.body.removeChild(divEl);
     });
 
-    it('.filter', function () {
-        var obj1 = {a: 1, b: 2};
-        var obj2 = collection.filter(obj1, function (val) {
-            return val > 1;
-        });
-        expect(obj1).not.toBe(obj2);
-        expect(obj1).toEqual({a: 1, b: 2});
-        expect(obj2).toEqual({b: 2});
+    var setStyle = function (el, cssKey, cssVal) {
+        el.style[cssKey] = cssVal;
+    };
 
-        var arr1 = ['a', 'b'];
-        var arr2 = collection.filter(arr1, function (val) {
-            return val.charCodeAt(0) > 97;
-        });
-        expect(arr1).not.toBe(arr2);
-        expect(arr1).toEqual(['a', 'b']);
-        expect(arr2).toEqual(['b']);
+    var getStyle = function (el, cssKey) {
+        return el.style[cssKey];
+    };
 
-        var boo1 = true;
-        var times = 0;
-        var boo2 = collection.filter(boo1, function () {
-            times++;
-            return '';
-        });
-        expect(times).toEqual(0);
-        expect(boo1).toBe(boo2);
+    it('.js', function () {
+        var key1 = compatible.js('document', window);
+        var key2 = compatible.js('IDBRequest', window);
+        var key3 = compatible.js('CSSMatrix', window);
+        var key4 = compatible.js('transitionEvent', window);
+
+        expect(key1 in window).toBe(true);
+        expect(key2 in window).toBe(true);
+        expect(key3 in window).toBe(true);
+        expect(key4 in window).toBe(true);
+    });
+
+    it('.event', function () {
+        var key1 = compatible.event('onabort', window);
+        var key2 = compatible.event('animationiteration', window);
+
+        key1 && expect('on' + key1 in window).toBe(true);
+        key2 && expect('on' + key2 in window).toBe(true);
+    });
+
+    it('.css', function () {
+        var ret1 = compatible.css('width');
+        var ret2 = compatible.css('display', 'flex');
+        var ret3 = compatible.css('border-radius', '2px');
+        var ret4 = compatible.css('filter', 'blur(2px)');
+
+        expect(ret1.key).toBe('width');
+        setStyle(divEl, ret1.key, '100px');
+        expect(getStyle(divEl, ret1.key)).toEqual('100px');
+
+        expect(ret2.key).toBe('display');
+        expect(ret2.val).toMatch(/flex/);
+        setStyle(divEl, ret2.key, ret2.val);
+        expect(getStyle(divEl, ret2.key)).toEqual(ret2.val);
+
+        if (ret3.key) {
+            expect(ret3.key).toMatch('border-radius');
+            expect(ret3.val).toMatch('2px');
+        }
+
+        // 某些浏览器不支持
+        if (ret4.key) {
+            expect(ret4.key).toMatch(/filter/i);
+            expect(ret4.val).toMatch(/blur/);
+            setStyle(divEl, ret4.key, ret4.val);
+            expect(getStyle(divEl, ret4.key)).toEqual(ret4.val);
+        }
     });
 });
